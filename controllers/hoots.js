60be1faf381cbf693eecc0a493299e1250c427a1
siseldyke <siseldyke@gmail.com>
2024-11-14 13:32:15 -0500

    all functions working

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -29,4 +29,88 @@ router.post('/', async (req, res) => {
     }
   });
 
+  router.get('/', async (req, res) => {
+    try {
+      const hoots = await Hoot.find({})
+        .populate('author')
+        .sort({ createdAt: 'desc' });
+      res.status(200).json(hoots);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+  
+
+router.get('/:hootId', async (req, res) => {
+    try {
+      const hoot = await Hoot.findById(req.params.hootId).populate('author');
+      res.status(200).json(hoot);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+  
+  router.put('/:hootId', async (req, res) => {
+    try {
+      // Find the hoot:
+      const hoot = await Hoot.findById(req.params.hootId);
+  
+      // Check permissions:
+      if (!hoot.author.equals(req.user._id)) {
+        return res.status(403).send("You're not allowed to do that!");
+      }
+  
+      // Update hoot:
+      const updatedHoot = await Hoot.findByIdAndUpdate(
+        req.params.hootId,
+        req.body,
+        { new: true }
+      );
+  
+      // Append req.user to the author property:
+      updatedHoot._doc.author = req.user;
+  
+      // Issue JSON response:
+      res.status(200).json(updatedHoot);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+  // controllers/hoots.js
+
+router.post('/:hootId/comments', async (req, res) => {
+    try {
+      req.body.author = req.user._id;
+      const hoot = await Hoot.findById(req.params.hootId);
+      hoot.comments.push(req.body);
+      await hoot.save();
+  
+      // Find the newly created comment:
+      const newComment = hoot.comments[hoot.comments.length - 1];
+  
+      newComment._doc.author = req.user;
+  
+      // Respond with the newComment:
+      res.status(201).json(newComment);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+  
+  // controllers/hoots.js
+
+router.put('/:hootId/comments/:commentId', async (req, res) => {
+    try {
+      const hoot = await Hoot.findById(req.params.hootId);
+      const comment = hoot.comments.id(req.params.commentId);
+      comment.text = req.body.text;
+      await hoot.save();
+      res.status(200).json({ message: 'Ok' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 module.exports = router;
