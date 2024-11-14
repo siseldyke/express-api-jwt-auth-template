875dc142055497500abee895c7fe95b48f068c84
siseldyke <siseldyke@gmail.com>
2024-11-14 13:21:51 -0500

    hoots can post

---
+++ controllers/hoots.js
@@ -0,0 +1,32 @@
+// controllers/hoots.js
+
+const express = require('express');
+const verifyToken = require('../middleware/verify-token.js');
+const Hoot = require('../models/hoot.js');
+const router = express.Router();
+
+// controllers/hoots.js
+
+
+  
+// ========== Public Routes ===========
+
+// ========= Protected Routes =========
+
+router.use(verifyToken);
+
+
+
+router.post('/', async (req, res) => {
+    try {
+      req.body.author = req.user._id;
+      const hoot = await Hoot.create(req.body);
+      hoot._doc.author = req.user;
+      res.status(201).json(hoot);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  });
+
+module.exports = router;
